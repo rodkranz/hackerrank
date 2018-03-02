47b49fef7780d6e7854c831d474fd2fe1830c36d
Rodrigo Lopes <rodrigo.lopes@olx.com>
2018-03-02 14:55:09 +0000

    change run

--- algorithm/strings/pangrams/run.sh
+++ algorithm/strings/pangrams/run.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e "We promptly judged antique ivory buckles for the next prize" | go run main.go
-#output=$(echo -e "We promptly judged antique ivory buckles for the next prize" | go run main.go)
-#echo "Must be [pangram = ${output}]"
-echo -e "We promptly judged antique ivory buckles for the prize" | go run main.go
-#output=$(echo -e "We promptly judged antique ivory buckles for the prize" | go run main.go)
-#echo "Must be [no pangram = ${output}]"
+output=$(echo -e "We promptly judged antique ivory buckles for the next prize" | go run main.go)
+echo "Must be [pangram = ${output}]"
+output=$(echo -e "We promptly judged antique ivory buckles for the prize" | go run main.go)
+echo "Must be [no pangram = ${output}]"
